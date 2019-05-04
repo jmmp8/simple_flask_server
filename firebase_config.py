8ca9eacd84d950c220ee4eaaa999166caf69e0fc
Jason Mao <jajamao11@gmail.com>
2019-05-04 16:04:36 -0700

    Add files via upload

---
+++ firebase_config.py
@@ -0,0 +1,7 @@
+config = {
+    'apiKey': None, #removed for security
+    'authDomain': "getrecipe-6ca89.firebaseapp.com",
+    'databaseURL': "https://getrecipe-6ca89.firebaseio.com",
+    'storageBucket': "getrecipe-6ca89.appspot.com",
+    'serviceAccount': './mycreds.json'
+}
